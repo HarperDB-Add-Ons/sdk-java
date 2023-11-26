f3e93d62e1f69f022726c1668c53fa53247a9e0b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-26 20:01:09 +0000

    chore: include mapper
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/JSONMapper.java
+++ core/src/main/java/expert/os/harperdb/JSONMapper.java
@@ -1,13 +1,17 @@
 package expert.os.harperdb;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public enum JSONMapper {
     INSTANCE;
 private final ObjectMapper mapper;
     {
         mapper = new ObjectMapper();
-
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     }
 
     public ObjectMapper getMapper() {
