0ac336e949b9b3fce533005eb301d2cb526dfb04
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-26 21:10:56 +0000

    docs: create documentation to operation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/Operation.java
+++ core/src/main/java/expert/os/harperdb/Operation.java
@@ -1,14 +1,27 @@
 package expert.os.harperdb;
 
+/**
+ * Abstract class representing a generic operation in HarperDB.
+ */
 abstract class Operation {
 
     private final OperationType operation;
 
+    /**
+     * Constructs a new Operation with the specified operation type.
+     *
+     * @param operation The type of the operation.
+     */
     Operation(OperationType operation) {
         this.operation = operation;
     }
 
-    public OperationType getOperation() {
+    /**
+     * Gets the type of the operation.
+     *
+     * @return The operation type.
+     */
+    public OperationType operation() {
         return operation;
     }
 }
