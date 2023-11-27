0d69773c3a6d962623086aa2b435ca1ceba6f4c1
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-27 19:04:20 +0000

    feat: create initial server structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/Server.java
+++ core/src/main/java/expert/os/harperdb/Server.java
@@ -1,2 +1,28 @@
-package expert.os.harperdb;public class Server {
+package expert.os.harperdb;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+
+public final class Server implements AutoCloseable {
+
+    private final URI host;
+    private final Auth auth;
+
+    private final HttpClient client;
+
+    private Server(URI host, Auth auth) {
+        this.host = host;
+        this.auth = auth;
+        this.client = HttpClient.newBuilder().build();
+    }
+
+    static Server of(URI host, Auth auth) {
+        return new Server(host, auth);
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.client.close();
+
+    }
 }
