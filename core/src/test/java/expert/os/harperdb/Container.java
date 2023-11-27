7b04130645b7640e3d5a2dfa1bf5f19c39ae17ea
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-27 19:26:17 +0000

    chore: move testcontainer to parent
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/test/java/expert/os/harperdb/Container.java
+++ core/src/test/java/expert/os/harperdb/Container.java
@@ -1,2 +1,9 @@
-package expert.os.harperdb;public enum Container {
+package expert.os.harperdb;
+
+public enum Container {
+    INSTANCE;
+
+    private final GenericContainer<?> harperDBContainer = new GenericContainer<>("harperdb/hdb")
+            .withExposedPorts(9925);
+
 }
--- core/src/test/java/expert/os/harperdb/ServerTest.java
+++ core/src/test/java/expert/os/harperdb/ServerTest.java
@@ -1,4 +1,7 @@
+package expert.os.harperdb;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ServerTest {
-  
+
 }
