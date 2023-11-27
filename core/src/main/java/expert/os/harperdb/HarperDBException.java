20eb4230e869f0d33e4fb2a580b998abea4ffe08
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-27 18:49:38 +0000

    feate: remove unsed method at harpddbexception
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/HarperDBException.java
+++ core/src/main/java/expert/os/harperdb/HarperDBException.java
@@ -40,17 +40,5 @@ public class HarperDBException extends RuntimeException {
         super(cause);
     }
 
-    /**
-     * Constructs a new HarperDBException with the specified detail message, cause, suppression enabled or
-     * disabled, and writable stack trace enabled or disabled.
-     *
-     * @param message            the detail message (which is saved for later retrieval by the getMessage() method).
-     * @param cause              the cause (which is saved for later retrieval by the getCause() method).
-     * @param enableSuppression whether or not suppression is enabled or disabled.
-     * @param writableStackTrace whether or not the stack trace should be writable.
-     */
-    public HarperDBException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
 
