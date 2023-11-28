afa35dbb45923cd8dd8e03cf6f1d5395a0e038af
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-28 18:30:29 +0000

    docs: include documentation at create database
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/CreateDatabase.java
+++ core/src/main/java/expert/os/harperdb/CreateDatabase.java
@@ -1,2 +1,32 @@
-package expert.os.harperdb;public class CreateDatabase {
+package expert.os.harperdb;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Represents the "create database" operation in HarperDB.
+ */
+final class CreateDatabase extends Operation {
+
+    @JsonProperty("database")
+    private final String database;
+
+    /**
+     * Constructs a new CreateDatabase operation with the specified database name.
+     *
+     * @param database The name of the database to be created.
+     */
+    CreateDatabase(String database) {
+        super(OperationType.CREATE_DATABASE);
+        this.database = database;
+    }
+
+    /**
+     * Gets the name of the database associated with this operation.
+     *
+     * @return The database name.
+     */
+    public String getDatabase() {
+        return database;
+    }
 }
+
