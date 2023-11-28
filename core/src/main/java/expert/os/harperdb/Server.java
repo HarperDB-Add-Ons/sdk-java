5e747cb0d50c45f025891667ed5f3b3fe8657237
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-28 06:18:25 +0000

    feat: create schema implementation to the server
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/Server.java
+++ core/src/main/java/expert/os/harperdb/Server.java
@@ -1,14 +1,22 @@
 package expert.os.harperdb;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Objects;
+
+import static expert.os.harperdb.JSONMapper.INSTANCE;
+import static java.net.http.HttpRequest.BodyPublishers.ofByteArray;
 
 /**
  * Represents a HarperDB server.
  */
 public final class Server  {
 
+    private static final int HTTP_STATUS_OK = 200;
     private final URI host;
     private final Auth auth;
 
@@ -30,8 +38,25 @@ public final class Server  {
      * @return true if the schema was created, false otherwise
      * @throws NullPointerException if schema is null
      */
-    public boolean createSchema(String schema) {
+    public boolean schema(String schema) {
         Objects.requireNonNull(schema, "schema is required");
+        HttpRequest request = createRequest().POST(ofByteArray(INSTANCE.writeValueAsBytes(new CreateSchema("test"))))
+                .build();
+
+        try {
+            HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+            return response.statusCode() == HTTP_STATUS_OK;
+        } catch (IOException| InterruptedException e) {
+            throw new HarperDBException("There is an issue to create the schema: " + schema, e);
+        }
+    }
+
+
+    private HttpRequest.Builder createRequest() {
+        return HttpRequest.newBuilder()
+                .uri(host)
+                .headers("Content-Type", "application/json;charset=UTF-8")
+                .header("Authorization", auth.header());
 
     }
 
--- core/src/test/java/expert/os/harperdb/ServerTest.java
+++ core/src/test/java/expert/os/harperdb/ServerTest.java
@@ -1,5 +1,7 @@
 package expert.os.harperdb;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,11 +11,32 @@ class ServerTest {
     private Container container = Container.INSTANCE;
 
     @Test
-    public void shouldGetHost() {
-        Server server = ServerBuilder.of(container.host())
+    void shouldGetHost() {
+        Server server = getServer();
+
+        Assertions.assertNotNull(server);
+    }
+
+    @Test
+    void shouldCreateSchema() {
+        Server server = getServer();
+
+        boolean schema = server.schema("test");
+        Assertions.assertTrue(schema);
+    }
+
+    @Test
+    void shouldNotCreateSchemaDuplicated() {
+        Server server = getServer();
+
+        boolean schema = server.schema("test");
+        Assertions.assertTrue(schema);
+        Assertions.assertFalse(server.schema("test"));
+    }
+
+    private Server getServer() {
+        return ServerBuilder.of(container.host())
                 .withCredentials(container.user(), container.password());
-
-
     }
 
 }
