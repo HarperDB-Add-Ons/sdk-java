bc3d9121ec6b76b528ea36bc40cdf94da5c2e8ff
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-28 06:07:55 +0000

    test: create server test scenario
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/test/java/expert/os/harperdb/ServerTest.java
+++ core/src/test/java/expert/os/harperdb/ServerTest.java
@@ -1,7 +1,19 @@
 package expert.os.harperdb;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServerTest {
+
+    private Container container = Container.INSTANCE;
+
+    @Test
+    public void shouldGetHost() {
+        Server server = ServerBuilder.of(container.host())
+                .withCredentials(container.user(), container.password());
+
+
+    }
 
 }
