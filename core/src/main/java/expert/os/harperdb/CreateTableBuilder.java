1e1d200dc7080a14353161e9ba219ceca76394b2
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-28 06:37:14 +0000

    feat: create builder to create table
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/CreateTableBuilder.java
+++ core/src/main/java/expert/os/harperdb/CreateTableBuilder.java
@@ -1,2 +1,38 @@
-package expert.os.harperdb;public class CreateTableBuilder {
+package expert.os.harperdb;
+
+import java.util.Objects;
+
+public final class CreateTableBuilder {
+
+    private final String schema;
+
+    private final Server server;
+
+    CreateTableBuilder(String schema, Server server) {
+        this.schema = schema;
+        this.server = server;
+    }
+
+
+    public TableBuilder table(String table) {
+        Objects.requireNonNull(table, "table is required");
+        return new TableBuilder(schema, table, server);
+    }
+
+
+    public static class TableBuilder {
+        private final String schema;
+        private final String table;
+        private final Server server;
+        private TableBuilder(String schema, String table, Server server) {
+            this.schema = schema;
+            this.table = table;
+            this.server = server;
+        }
+        public boolean id(String id) {
+            Objects.requireNonNull(id, "id is required");
+            return false;
+        }
+    }
+
 }
