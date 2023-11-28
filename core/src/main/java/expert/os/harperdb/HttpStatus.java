cbca42844d1ec3f3e921c1220149b26dd4b48681
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-28 06:22:03 +0000

    docs: create documentation to httpstatus
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/HttpStatus.java
+++ core/src/main/java/expert/os/harperdb/HttpStatus.java
@@ -2,15 +2,32 @@ package expert.os.harperdb;
 
 import java.util.function.Supplier;
 
-public enum HttpStatus implements Supplier<Integer> {
+/**
+ * Enum representing HTTP status codes.
+ * Each constant in this enum corresponds to a specific HTTP status code.
+ */
+enum HttpStatus implements Supplier<Integer> {
+    /**
+     * HTTP status code 200 OK.
+     */
     OK(200);
 
     private final int status;
 
+    /**
+     * Constructs an HttpStatus enum constant with the specified status code.
+     *
+     * @param status The HTTP status code.
+     */
     HttpStatus(int status) {
         this.status = status;
     }
 
+    /**
+     * Gets the integer representation of the HTTP status code.
+     *
+     * @return The HTTP status code.
+     */
     @Override
     public Integer get() {
         return status;
