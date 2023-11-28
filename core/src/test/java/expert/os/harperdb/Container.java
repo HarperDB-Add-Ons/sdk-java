bcb3228f2ec67aa02efd85dc97775dc33324a6a3
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-28 06:07:43 +0000

    feat: create container for test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/test/java/expert/os/harperdb/Container.java
+++ core/src/test/java/expert/os/harperdb/Container.java
@@ -1,9 +1,35 @@
 package expert.os.harperdb;
 
+import org.testcontainers.containers.GenericContainer;
+
+import java.util.List;
+
 public enum Container {
     INSTANCE;
 
-    private final GenericContainer<?> harperDBContainer = new GenericContainer<>("harperdb/hdb")
-            .withExposedPorts(9925);
+    private static final String PASSWORD = "password";
+    private static final String USER = "root";
+    private final GenericContainer<?> container = new GenericContainer<>("harperdb/harperdb")
+            .withExposedPorts(9925).withEnv("HDB_ADMIN_PASSWORD", PASSWORD)
+            .withEnv("HDB_ADMIN_USERNAME", USER);
+
+    {
+        container.start();
+    }
+
+    public String host() {
+        return "http://" + container.getContainerIpAddress() + ":" + container.getMappedPort(9925);
+    }
+
+    public List<String> envs() {
+        return container.getEnv();
+    }
+
+    public String user() {
+        return USER;
+    }
+    public String password() {
+        return PASSWORD;
+    }
 
 }
