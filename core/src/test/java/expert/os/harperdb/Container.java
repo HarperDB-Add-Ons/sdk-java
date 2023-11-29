be8654643cb129990d1d977e66ee06708f1f6a9a
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-29 16:10:38 +0000

    style: update header license
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/test/java/expert/os/harperdb/Container.java
+++ core/src/test/java/expert/os/harperdb/Container.java
@@ -1,3 +1,20 @@
+/*
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Contributors to the HarperDB
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License, which accompanies
+ * this distribution. The full text of the license may be found at
+ * https://opensource.org/licenses/MIT.
+ *
+ * You may elect to redistribute this code under the MIT License.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ */
+
 package expert.os.harperdb;
 
 import org.testcontainers.containers.GenericContainer;
