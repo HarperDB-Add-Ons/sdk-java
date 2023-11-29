436f750f9a01b9ccc78d38f33667299d5440e0ae
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-29 05:28:26 +0000

    docs: create documentation upsert
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/Upsert.java
+++ core/src/main/java/expert/os/harperdb/Upsert.java
@@ -3,7 +3,12 @@ package expert.os.harperdb;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Represents the "upsert" operation in HarperDB.
+ * This operation is used to insert or update records within a specific table in a given database.
+ */
 final class Upsert extends Operation{
 
     @JsonProperty
@@ -13,10 +18,45 @@ final class Upsert extends Operation{
     @JsonProperty
     private final List<?> records;
 
+    /**
+     * Constructs a new Upsert operation with the specified database, table, and records.
+     *
+     * @param database The name of the database where records will be inserted or updated.
+     * @param table    The name of the table where records will be inserted or updated.
+     * @param records  The list of records containing data to be upserted into the table.
+     */
     Upsert(String database, String table, List<?> records) {
         super(OperationType.UPSERT);
         this.database = database;
         this.table = table;
         this.records = records;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Upsert upsert = (Upsert) object;
+        return Objects.equals(database, upsert.database)
+                && Objects.equals(table, upsert.table)
+                && Objects.equals(records, upsert.records);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(database, table, records);
+    }
+
+    @Override
+    public String toString() {
+        return "Upsert{" +
+                "database='" + database + '\'' +
+                ", table='" + table + '\'' +
+                ", records=" + records +
+                '}';
     }
 }
