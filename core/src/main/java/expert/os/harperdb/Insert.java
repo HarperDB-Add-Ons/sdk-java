faf8fb72daf58849737a835d1a0d8f4ae9db48f0
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-29 05:25:36 +0000

    docs: create documentation at insert
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/Insert.java
+++ core/src/main/java/expert/os/harperdb/Insert.java
@@ -3,7 +3,12 @@ package expert.os.harperdb;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Represents the "insert" operation in HarperDB.
+ * This operation is used to insert records into a specific table within a specified database.
+ */
 final class Insert extends Operation{
 
     @JsonProperty
@@ -13,10 +18,45 @@ final class Insert extends Operation{
     @JsonProperty
     private final List<?> records;
 
+    /**
+     * Constructs a new Insert operation with the specified database, table, and records.
+     *
+     * @param database The name of the database where records will be inserted.
+     * @param table    The name of the table where records will be inserted.
+     * @param records  The list of records to be inserted into the table.
+     */
     Insert(String database, String table, List<?> records) {
         super(OperationType.INSERT);
         this.database = database;
         this.table = table;
         this.records = records;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Insert insert = (Insert) object;
+        return Objects.equals(database, insert.database)
+                && Objects.equals(table, insert.table)
+                && Objects.equals(records, insert.records);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(database, table, records);
+    }
+
+    @Override
+    public String toString() {
+        return "Insert{" +
+                "database='" + database + '\'' +
+                ", table='" + table + '\'' +
+                ", records=" + records +
+                '}';
     }
 }
