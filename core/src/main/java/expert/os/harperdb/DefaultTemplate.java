102cc79c18a3f2ede2ac32961d0d75caea637bc1
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-29 16:40:24 +0000

    feat: hide the default implementation of template2
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- core/src/main/java/expert/os/harperdb/DefaultTemplate.java
+++ core/src/main/java/expert/os/harperdb/DefaultTemplate.java
@@ -26,7 +26,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-public final class DefaultTemplate implements Template {
+final class DefaultTemplate implements Template {
     private static final Set<String> ALL_ATTRIBUTES = Collections.singleton("*");
     private final String database;
     private final Server server;
